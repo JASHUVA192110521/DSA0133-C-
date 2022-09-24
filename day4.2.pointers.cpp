70ce05f03e27493d617e8191822855b09d1fe3e8
JASHUVA192110521 <112766506+JASHUVA192110521@users.noreply.github.com>
2022-09-24 13:23:07 +0530

    Create day4.2.pointers.cpp

---
+++ day4.2.pointers.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+class A
+{
+	int *r,d;
+	float pi,a;
+	public:
+		
+		void cal()
+		{
+			r=&d;
+		
+			cout<<"enter the number=";
+			cin>>d;
+			
+			cout<<r;
+			pi=3.14;
+			a=pi*d*d;
+		}
+		p()
+		{
+			cout<<"Area of circle="<<a;
+		}
+};
+int main()
+{
+	
+	A *r;
+	r=&d;
+
+	A t; 
+	t.cal();
+	t.p();
+	return 0;
+
+	
+}
